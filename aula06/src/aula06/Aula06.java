9e136ab11ba2906da404e971eca0f214ee8d50ad
Robson de Arruda <robsonshockwavegithub@gmail.com>
2020-10-22 17:08:54 -0300

    Encapsulamento

---
+++ aula06/src/aula06/Aula06.java
@@ -0,0 +1,15 @@
+package aula06;
+
+//Encapsulamento
+public class Aula06 {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
