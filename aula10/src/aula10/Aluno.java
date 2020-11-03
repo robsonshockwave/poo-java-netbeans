82e89c6a5812d23f21063f0a1a308b2b5be7e522
Robson de Arruda <robsonshockwavegithub@gmail.com>
2020-11-03 10:34:54 -0300

    Herança parte 1

---
+++ aula10/src/aula10/Aluno.java
@@ -0,0 +1,28 @@
+package aula10;
+
+public class Aluno extends Pessoa {
+    private int mat;
+    private String curso;
+    
+    public void cancelarMatr() {
+        System.out.println("Matrícula foi cancelada!");
+    }
+
+    public int getMat() {
+        return mat;
+    }
+
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    
+}
